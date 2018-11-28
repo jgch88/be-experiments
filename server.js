85ae00df1c17a6c7499b82ac37b7e9ee164c05e9
jgch88 <radium.laboratories@gmail.com>
2018-11-28 20:15:44 +0800

    added eslint

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 
 const app = express();
+
+const PORT = 3000;
+
 app.get('/', (req, res) => {
   res.status(200).send('ok');
 });
 
-const server = app.listen(3000, () => {
-  port = server.address().port;
+const server = app.listen(PORT, () => {
+  const [port] = server.address();
   console.log(`Listening at port ${port}`);
 });
 
-
-
 module.exports = app;
-
-
